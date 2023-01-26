c011eae699b960fe60aa2af412bdd1c2d1a66a5d
m4koo <mahmut.kalem@outlook.de>
2023-01-26 14:48:07 +0100

    functionality of selecting right and wrong answers with animation

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let htmlQuestions = [
         "answer_2": "Hyper Text Markup Language",
         "answer_3": "Hey Too Much Layout",
         "answer_4": "Hieroglyphics, To Machine Legible",
-        "correct_answer": 3,
+        "correct_answer": 2,
     },
     {
         "question": "Wer ist für die Web standards zuständig?",
@@ -41,8 +41,76 @@ let htmlQuestions = [
     },
 ];
 
+let currentQuestion = 0;
+
 function showQuestion(){
     let card = document.getElementById('mainCard');
     card.innerHTML="";
+    generateQuestionHTML(card);
+}
+
+function answerSelection(id){
+    let selected = document.getElementById(id);
+    let rightAnswer = "answer_" + htmlQuestions[currentQuestion]['correct_answer']
+    console.log(rightAnswer);
+    // let answer = id.slice(-1);
+    if (id == rightAnswer){
+        console.log("yes");
+        selected.style.backgroundColor = "rgb(197, 244, 163)";
+        animRight(selected);
+    }else{
+        console.log("no");
+        selected.style.backgroundColor = "rgb(243, 167, 166)";
+        animWrong(selected);
+        document.getElementById(rightAnswer).style.background = "rgb(197, 244, 163)"
+    }
+}
+
+function animRight(selected){
+    selected.style.transition = "transform 300ms ease-in-out";
+    selected.style.transform = "scale(1.1)";
+    setTimeout(() => {
+        selected.style.transform = "scale(1)";
+    }, 300);
+}
+
+function animWrong(selected){
+    selected.style.transition = "transform 0.3s ease-in-out";
+    selected.style.transformOrigin = "center bottom";
+  
+    for (let i = 0; i < 2; i++) {
+        setTimeout(() => {
+            selected.style.transform = "rotate(-10deg)";
+        }, i * 250);
+        setTimeout(() => {
+            selected.style.transform = "rotate(10deg)";
+        }, i * 250 + 125);
+    }
+  
+    setTimeout(() => {
+        selected.style.transform = "none";
+    }, 500);
+}
+
+function generateQuestionHTML(id){
+    id.innerHTML =`
+    <h2 class="my-5 fw-bold">${htmlQuestions[currentQuestion]['question']}</h2>
+    <div class="d-flex gap-4 answer-choice" id="answer_1" onclick="answerSelection('answer_1')">
+        <span>A</span>
+        <p>${htmlQuestions[currentQuestion]['answer_1']}</p>
+     </div>
+    <div class="d-flex gap-4 answer-choice" id="answer_2" onclick="answerSelection('answer_2')">
+        <span>B</span>
+        <p>${htmlQuestions[currentQuestion]['answer_2']}</p>
+    </div>
+    <div class="d-flex gap-4 answer-choice" id="answer_3" onclick="answerSelection('answer_3')">
+        <span>C</span>
+        <p>${htmlQuestions[currentQuestion]['answer_3']}</p>
+    </div>
+    <div class="d-flex gap-4 answer-choice" id="answer_4" onclick="answerSelection('answer_4')">
+        <span>D</span>
+        <p>${htmlQuestions[currentQuestion]['answer_4']}</p>
+    </div>
+    `
 }
 
